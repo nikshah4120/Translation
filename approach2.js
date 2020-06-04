b8c1741174286db110190b9032a218e482dce274
nikshah4120 <50862248+nikshah4120@users.noreply.github.com>
2020-06-04 13:21:33 +0530

    Fix

--- approach2.js
+++ approach2.js
@@ -1,41 +1,59 @@
-const jsonfile=require('jsonfile');
-const fs=require('fs');
-const path=require('path');
+const jsonfile = require('jsonfile');
+const fs = require('fs');
+const path = require('path');
+const sortJson = require('sort-json');
 
-const folderName='translations'; // Name of folder where all locales are kept.
-const mainFileName='en_US.json'; //Main File in which all labels are kept which are nedded to be translated.
-const outputFileName="output_en_US.json"; //Output File where untranslated label will be placed.
-var outputObject={};
-const folderPath=path.resolve(__dirname,folderName);  //Path of folder where all json file of loacales kept.
-const mainFilePath=path.resolve(folderPath,mainFileName); //Path of en_US.json File
-var fileNames=fs.readdirSync(folderPath); 
-var numberOfFiles=fileNames.length;
+const folderName = 'translations'; // Name of folder where all locales are kept.
+const mainFileName = 'en_US.json'; //Main File in which all labels are kept which are nedded to be translated.
+var outputFileName = "output_en_US.json"; //Output File where untranslated label will be placed.
+var outputObject = {}; //Output Object will contain all untranslate label (Label is considered as untranslated if it is not translated in any one language)
+const folderPath = path.resolve(__dirname, folderName); //Path of folder where all json file of loacales kept.
+const mainFilePath = path.resolve(folderPath, mainFileName); //Path of en_US.json File
+
+/*Optional Output Path  (Uncomment below part if you want to create output file in specific folder)*/
+
+/*
+const outputFolderName="translations";
+outputFileName=path.resolve(__dirname,outputFolderName,outputFileName);
+*/
+
+var files = fs.readdirSync(folderPath); //Extracting file names from the folder.                
+var numberOfFiles = files.length; // count of number of files
+
+
+var fileNames = files.filter((filename) => filename !== mainFileName); //Removing en_US.json file from the list since we are processing it seperately.
 
 /* Reading and extracting labels */
-var labelList=Object.keys(jsonfile.readFileSync(mainFilePath));
 
-//var labelList=Object.keys(data); // List of all labels;
-var isExist={}; //holds count as value where count=number of different files where label is present.
-labelList.forEach((label)=>{
-    isExist[label]= 1;                                                                      
+var labelList = Object.keys(jsonfile.readFileSync(mainFilePath));
+
+var isExist = {}; //holds label as key and  count as value where count is number of different files where label is present.
+labelList.forEach((label) => {
+    isExist[label] = 1; //Setting count=1 since it is present in en_US.json file                                             
 })
 
 /* Checking whether label exists in translated file or not . */
-fileNames.forEach(file=>{
-    let checkFile=path.resolve(folderPath,file); //Path to the file of different languages
-    if(checkFile!== mainFilePath){
-        data=Object.keys(jsonfile.readFileSync(checkFile));    
-        data.forEach(item => {
-            isExist[item]+=1;
-        }); 
-    }
+fileNames.forEach(file => {
+    let checkFile = path.resolve(folderPath, file); //Path of the file of different language
+    data = Object.keys(jsonfile.readFileSync(checkFile)); //Extracting label from the corresponding file
+    data.forEach(item => {
+        isExist[item] += 1; //Incremetning count of label since it is present in file.
+    });
 });
+
 labelList.forEach(label => {
-    if(isExist[label] !== numberOfFiles){            //If label is not present in all files will be added in output file
-        outputObject[label] = label;       
+    if (isExist[label] !== numberOfFiles) { //If label is not present in all files will be added in output file
+        outputObject[label] = label;
     }
 });
 
 
-/* Creating output File */
-jsonfile.writeFileSync(outputFileName,outputObject,{spaces : 1})
+/* Creating output File Note:(If you want to create output file in specific folder replace outputFileName to the outputFilePath and refer line no 13 to 19) */
+jsonfile.writeFileSync(outputFileName, outputObject, {
+    spaces: 1
+})
+
+/*Sorting output json File ignoring case */
+sortJson.overwrite(outputFileName, {
+    ignoreCase: true
+});
---
+++ d
@@ -0,0 +1,43 @@
+[33mcommit 7403a8a24d3c1c3c695c9e54287b00c11d2d5a50[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m)[m
+Author: nikshah4120 <50862248+nikshah4120@users.noreply.github.com>
+Date:   Thu Jun 4 12:10:54 2020 +0530
+
+    Fix and Beautification
+
+[33mcommit 4ade6d49cc3061da28ceba29d50b6b26c432e14d[m
+Author: nikshah4120 <50862248+nikshah4120@users.noreply.github.com>
+Date:   Thu Jun 4 12:01:10 2020 +0530
+
+    Code Beautification Done
+
+[33mcommit 3b059b1d0e32abc1b98a7c4cd03b74006bbd1f1d[m
+Author: nikshah4120 <50862248+nikshah4120@users.noreply.github.com>
+Date:   Wed Jun 3 17:52:31 2020 +0530
+
+    Revert "Updates"
+    
+    This reverts commit 18549cc79229aa6eb81beca68111cac42f2aecbd.
+
+[33mcommit 18549cc79229aa6eb81beca68111cac42f2aecbd[m
+Author: nikshah4120 <50862248+nikshah4120@users.noreply.github.com>
+Date:   Wed Jun 3 17:43:34 2020 +0530
+
+    Updates
+
+[33mcommit b37414ad8d97d3f354408f478585254e2006cb11[m[33m ([m[1;31morigin/master[m[33m)[m
+Author: nikshah4120 <50862248+nikshah4120@users.noreply.github.com>
+Date:   Wed Jun 3 17:35:23 2020 +0530
+
+    Improvement and Syntax fix
+
+[33mcommit ab8ca49c83b226545640f4c43a37dba3fc779bff[m
+Author: nikshah4120 <50862248+nikshah4120@users.noreply.github.com>
+Date:   Wed Jun 3 13:25:41 2020 +0530
+
+    Optimized Approach
+
+[33mcommit 0ade73247cbad12b6c057d99f8cd33ad04d0c581[m
+Author: nikshah4120 <50862248+nikshah4120@users.noreply.github.com>
+Date:   Tue Jun 2 19:01:05 2020 +0530
+
+    First commit
